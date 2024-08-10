01b2584efd65dc6972956a4b95e932636d8e2854
Cooper-VR <cooperjbower@gmail.com>
2024-08-10 13:28:12 -0700

    found left and right controller

--- vr-wheel-emulator/vr-wheel-emulator.cpp
+++ vr-wheel-emulator/vr-wheel-emulator.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include "headers/openvr.h"
+//#include "headers/openvr_driver.h"
 
 int main()
 {
+    vr::EVRInitError eError = vr::VRInitError_None;
+    vr::IVRSystem* pSystem = vr::VR_Init(&eError, vr::VRApplication_Background);
+
+    if (eError != vr::VRInitError_None) {
+        std::cout << "Failed to iniitalize, is SteamVR running\n";
+        return -1;
+    }
+    std::cout << "OpenVR initalized\n";
+
+    //std::cout << pSystem->IsTrackedDeviceConnected(vr::k_unTrackedDeviceIndex_Hmd+1) << '\n';
+
+    vr::ETrackedDeviceProperty deviceProp = vr::Prop_RenderModelName_String;
+
+    vr::ETrackedPropertyError dError;
+    int BufferSize = 256;
+    char *propString = new char[BufferSize];
+    
+    //for me  +3 is left controller, +4 is right, +1||+2 are base stations
+    pSystem->GetStringTrackedDeviceProperty(vr::k_unTrackedDeviceIndex_Hmd + 4, deviceProp, propString, BufferSize, &dError) << '\n';
+    if (dError != vr::TrackedProp_Success) {
+        std::cout << "failed?: " << dError;
+        return -2;
+    }
+    std::string trackingSystemName(propString);
+
+    std::cout << trackingSystemName << '\n';
+
     std::cout << "Hello World!\n";
 }
