0ba625f6e420bf2ab09deec79480a48bad69a2cb
Cooper-VR <cooperjbower@gmail.com>
2024-08-10 13:48:24 -0700

    got right rotation

--- vr-wheel-emulator/vr-wheel-emulator.cpp
+++ vr-wheel-emulator/vr-wheel-emulator.cpp
@@ -28,8 +28,29 @@ int main()
         return -2;
     }
     std::string trackingSystemName(propString);
-
     std::cout << trackingSystemName << '\n';
+    //can use render models ot tell difference
+
+    vr::TrackedDevicePose_t trackedDevicesPose[vr::k_unMaxTrackedDeviceCount];
+
+    pSystem->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0, trackedDevicesPose, vr::k_unMaxTrackedDeviceCount);
+
+    if (trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].bPoseIsValid) {
+        vr::HmdMatrix34_t matPose = trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].mDeviceToAbsoluteTracking;
+
+        // Extract rotation (in the form of a quaternion) from the matrix
+        float qw = sqrt(fmax(0, 1 + matPose.m[0][0] + matPose.m[1][1] + matPose.m[2][2])) / 2;
+        float qx = sqrt(fmax(0, 1 + matPose.m[0][0] - matPose.m[1][1] - matPose.m[2][2])) / 2;
+        float qy = sqrt(fmax(0, 1 - matPose.m[0][0] + matPose.m[1][1] - matPose.m[2][2])) / 2;
+        float qz = sqrt(fmax(0, 1 - matPose.m[0][0] - matPose.m[1][1] + matPose.m[2][2])) / 2;
+
+        qx = copysign(qx, matPose.m[2][1] - matPose.m[1][2]);
+        qy = copysign(qy, matPose.m[0][2] - matPose.m[2][0]);
+        qz = copysign(qz, matPose.m[1][0] - matPose.m[0][1]);
+
+        std::cout << "rotation: " << qx << ' ' << qy << ' ' << qz << '\n';
+    }
+
 
     std::cout << "Hello World!\n";
 }
