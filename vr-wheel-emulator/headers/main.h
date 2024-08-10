f4d5f665a55b7011d13dc7c5bae8c7af819f0aee
Cooper-VR <cooperjbower@gmail.com>
2024-08-10 14:31:09 -0700

    got rotation of right controller

---
+++ vr-wheel-emulator/headers/main.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include "openvr.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/euler_angles.hpp>
+
+const double PI = 3.14159265358979323846;
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+Vector3 GetRotation(vr::IVRSystem *pSystem) {
+    vr::TrackedDevicePose_t trackedDevicesPose[vr::k_unMaxTrackedDeviceCount];
+
+    pSystem->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0, trackedDevicesPose, vr::k_unMaxTrackedDeviceCount);
+
+    if (trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].bPoseIsValid) {
+        vr::HmdMatrix34_t mat = trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].mDeviceToAbsoluteTracking;
+
+        // Convert the 3x4 matrix to a 4x4 matrix
+        glm::mat4 matrix = glm::mat4(1.0f);
+        matrix[0][0] = mat.m[0][0]; matrix[1][0] = mat.m[0][1]; matrix[2][0] = mat.m[0][2]; matrix[3][0] = 0.0f;
+        matrix[0][1] = mat.m[1][0]; matrix[1][1] = mat.m[1][1]; matrix[2][1] = mat.m[1][2]; matrix[3][1] = 0.0f;
+        matrix[0][2] = mat.m[2][0]; matrix[1][2] = mat.m[2][1]; matrix[2][2] = mat.m[2][2]; matrix[3][2] = 0.0f;
+        matrix[0][3] = mat.m[0][3]; matrix[1][3] = mat.m[1][3]; matrix[2][3] = mat.m[2][3]; matrix[3][3] = 1.0f;
+
+        // Extract yaw, pitch, and roll from the matrix (use glm or your preferred math library)
+        glm::vec3 euler = glm::eulerAngles(glm::quat_cast(matrix));
+
+        float yaw = glm::degrees(euler.y);
+        float pitch = glm::degrees(euler.x);
+        float roll = glm::degrees(euler.z);
+
+        // Now you have yaw, pitch, and roll in degrees
+
+        Vector3 Rotation;
+        Rotation.x = yaw;
+        Rotation.y = pitch;
+        Rotation.z = roll;
+        return Rotation;
+    }
+}
+
+void PrintRotation(Vector3 rotation) {
+	std::cout << "rotation: " << rotation.x << ' ' << rotation.y << ' ' << rotation.z << '\n';
+}
+
+void ConvertToDegrees(Vector3* Rotation) {
+    Rotation->x *= (180 / (float)PI);
+    Rotation->y *= (180 / (float)PI);
+    Rotation->z *= (180 / (float)PI);
+}
--- vr-wheel-emulator/vr-wheel-emulator.cpp
+++ vr-wheel-emulator/vr-wheel-emulator.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "headers/openvr.h"
 //#include "headers/openvr_driver.h"
+#include "headers/main.h"
+
+
 
 int main()
 {
@@ -33,24 +36,13 @@ int main()
 
     vr::TrackedDevicePose_t trackedDevicesPose[vr::k_unMaxTrackedDeviceCount];
 
-    pSystem->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0, trackedDevicesPose, vr::k_unMaxTrackedDeviceCount);
+    Vector3 rotation;
 
-    if (trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].bPoseIsValid) {
-        vr::HmdMatrix34_t matPose = trackedDevicesPose[vr::k_unTrackedDeviceIndex_Hmd + 4].mDeviceToAbsoluteTracking;
-
-        // Extract rotation (in the form of a quaternion) from the matrix
-        float qw = sqrt(fmax(0, 1 + matPose.m[0][0] + matPose.m[1][1] + matPose.m[2][2])) / 2;
-        float qx = sqrt(fmax(0, 1 + matPose.m[0][0] - matPose.m[1][1] - matPose.m[2][2])) / 2;
-        float qy = sqrt(fmax(0, 1 - matPose.m[0][0] + matPose.m[1][1] - matPose.m[2][2])) / 2;
-        float qz = sqrt(fmax(0, 1 - matPose.m[0][0] - matPose.m[1][1] + matPose.m[2][2])) / 2;
-
-        qx = copysign(qx, matPose.m[2][1] - matPose.m[1][2]);
-        qy = copysign(qy, matPose.m[0][2] - matPose.m[2][0]);
-        qz = copysign(qz, matPose.m[1][0] - matPose.m[0][1]);
-
-        std::cout << "rotation: " << qx << ' ' << qy << ' ' << qz << '\n';
+    while (1) {
+        rotation = GetRotation(pSystem);
+        //ConvertToDegrees(&rotation);
+        PrintRotation(rotation);
     }
-
 
     std::cout << "Hello World!\n";
 }
